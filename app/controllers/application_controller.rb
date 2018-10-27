fb801cde1e0d51e2b736f1fab683107ebe5d6c8b
Michael Roose <mjroose@gmail.com>
2018-10-27 20:37:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,10 @@ class App < Sinatra::Base
       @team = Team.new({name: params[:team][:name], motto: params[:team][:motto]})
 
       @heroes = params[:team][:members].collect do |team_member|
-        @team[:members] << Hero.new(team_member)
+        @team.members << Hero.new(team_member)
       end
       binding.pry
-      
+
       erb :teams
     end
 end
