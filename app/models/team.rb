1dbb35af0c2571fd477e6ae4713a6112057e45bc
Michael Roose <mjroose@gmail.com>
2018-10-27 20:05:41 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,9 @@
 class Team
   attr_accessor :name, :motto, :members
+
+  def initialize(team_data = {})
+    @name = team_data[:name]
+    @motto = team_data[:motto]
+    @members = team_data[:members]
+  end
 end
